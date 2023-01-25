ad8ca69f6252e0c45aadc10f00e17ae85b05ee3c
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-01-25 21:43:59 +0100

    Stylization
    Finished the look

--- JukeBoxCode.java
+++ JukeBoxCode.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -10,6 +11,8 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class JukeBoxCode extends JFrame implements ActionListener{
     
@@ -18,6 +21,8 @@ public class JukeBoxCode extends JFrame implements ActionListener{
     JButton btn3=new JButton("Badland-Boom Kitty");
     JButton btn4=new JButton("Firewall-Boom Kitty");
     JButton btn5=new JButton("Law of the jungle-Boom Kitty");
+    JPanel background=new JPanel();
+    JLabel nowPlaying=new JLabel();
 
 
     JukeBoxCode(){
@@ -25,32 +30,57 @@ public class JukeBoxCode extends JFrame implements ActionListener{
         this.setVisible(true);
         this.setLayout(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setBackground(Color.BLACK);
 
         btn1.setSize(200,30);
-        btn1.setLocation(100,100);
+        btn1.setLocation(200,100);
         btn1.addActionListener(this);
-
+        btn1.setFocusable(false);
+        btn1.setBackground(Color.gray);
+        btn1.setForeground(Color.white);
+        
         btn2.setSize(200,30);
-        btn2.setLocation(100,150);
+        btn2.setLocation(200,150);
         btn2.addActionListener(this);
+        btn2.setFocusable(false);
+        btn2.setBackground(Color.gray);
+        btn2.setForeground(Color.yellow);
 
         btn3.setSize(200,30);
-        btn3.setLocation(100,200);
+        btn3.setLocation(200,200);
         btn3.addActionListener(this);
+        btn3.setFocusable(false);
+        btn3.setBackground(Color.gray);
+        btn3.setForeground(Color.red);
 
         btn4.setSize(200,30);
-        btn4.setLocation(100,250);
+        btn4.setLocation(200,250);
         btn4.addActionListener(this);
+        btn4.setFocusable(false);
+        btn4.setBackground(Color.gray);
+        btn4.setForeground(Color.orange);
 
         btn5.setSize(200,30);
-        btn5.setLocation(100,300);
+        btn5.setLocation(200,300);
         btn5.addActionListener(this);
+        btn5.setFocusable(false);
+        btn5.setBackground(Color.gray);
+        btn5.setForeground(Color.green);
+
+        nowPlaying.setSize(250,30);
+        nowPlaying.setLocation(200,400);
+
+        background.setSize(600,600);
+        background.setLocation(0, 0);
+        background.setBackground(Color.black);
 
         this.add(btn1);
         this.add(btn2);
         this.add(btn3);
         this.add(btn4);
         this.add(btn5);
+        this.add(nowPlaying);
+        this.add(background);
     }
 
     @Override
@@ -59,22 +89,32 @@ public class JukeBoxCode extends JFrame implements ActionListener{
     
     if(e.getSource()==btn1){
         song1();
+        nowPlaying.setText("Now playing: Deo Volente");
+        nowPlaying.setForeground(Color.white);
     }
     
     else if(e.getSource()==btn2){
         song2();
+        nowPlaying.setText("Now playing: Holy war");
+        nowPlaying.setForeground(Color.yellow);
     }
     
     else if(e.getSource()==btn3){
         song3();
+        nowPlaying.setText("Now playing: Badland");
+        nowPlaying.setForeground(Color.red);
     }
     
     else if(e.getSource()==btn4){
         song4();
+        nowPlaying.setText("Now playing: Firewall");
+        nowPlaying.setForeground(Color.orange);
     }
     
     else if(e.getSource()==btn5){
         song5();
+        nowPlaying.setText("Now playing: Law of the Jungle");
+        nowPlaying.setForeground(Color.green);
     }
 
     }
